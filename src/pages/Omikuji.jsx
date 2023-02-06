36dd95e21eeef0c3662f6392ba65d56ac44f64fd
Kentaro Sato <stkntr@gmail.com>
2023-02-06 20:16:02 +0900

    useState の活用

--- src/pages/Omikuji.jsx
+++ src/pages/Omikuji.jsx
@@ -1,17 +1,22 @@
+import { useState } from "react";
 import { ActionButton } from "../components/ActionButton";
 
 export const Omikuji = () => {
+  const [omikujiResult, setOmikujiResult] = useState("");
+
   const getOmikuji = () => {
     const result = ["大吉", "中吉", "小吉", "凶", "大凶"][
       Math.floor(Math.random() * 5)
     ];
     console.log(result);
+    setOmikujiResult(result);
   };
 
   return (
     <>
       <p>おみくじの画面</p>
       <ActionButton text="おみくじをひく" action={getOmikuji} />
+      <p>{omikujiResult}</p>
     </>
   );
 };
