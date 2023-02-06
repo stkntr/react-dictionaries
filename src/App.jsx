0b380302a85e8dee09c9bce377f83b03177f568e
Kentaro Sato <stkntr@gmail.com>
2023-02-06 17:06:51 +0900

    ActionButtonコンポーネントの作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { ActionButton } from "./components/ActionButton";
+
 const App = () => {
   return (
     <>
       <h1>react app</h1>
+      <ActionButton />
     </>
   );
 };
---
+++ src/components/ActionButton.jsx
@@ -0,0 +1,7 @@
+export const ActionButton = () => {
+  return (
+    <>
+      <button type="button">おみくじをひく</button>
+    </>
+  );
+};
