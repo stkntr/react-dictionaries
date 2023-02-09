661471d7431ea3daa1459cd2a569794929dc1c08
Kentaro Sato <stkntr@gmail.com>
2023-02-09 21:48:15 +0900

    404に対応

--- src/pages/English-English.jsx
+++ src/pages/English-English.jsx
@@ -18,46 +18,61 @@ export const EnglishEnglish = () => {
   const getDictString = (json) => {
     // const dictString = json.length;
     console.log(json);
+    console.log(json instanceof Array);
+
     let dictString = "";
-    try {
-      let dictStrings = [];
-      for (let i=0; i<json.length; i++) {
-        let dict = json[i];
-        let dictString = "";
-        if (json.length > 1) {
-          let supNumber = i+1;
-          // supNumber = superscript(supNumber);
-          dictString = "<b>" + dict.word + "<sup>" + supNumber + "</sup>" + "</b>";
-        } else {
-          dictString = "<b>" + dict.word + "</b>";
-        }
-        if ("phonetic" in dict) {
-          dictString += " " + dict.phonetic;
-        }
-        for (let j=0; j<dict.meanings.length; j++) {
-          let meaning = dict.meanings[j];
-          dictString += "<br>(" + meaning.partOfSpeech + ")";
-          for (let k=0; k<meaning.definitions.length; k++) {
-            let currentDefinition = meaning.definitions[k];
-            let definition = currentDefinition.definition;
-            if (meaning.definitions.length > 1) {
-              let definitionNumber = k + 1;
-              dictString += "&ensp;<b>" + definitionNumber + ".</b> " + definition + "";
-            } else {
-              dictString += "&ensp;" + definition + "";
-            }
-            if ("example" in currentDefinition) {
-              dictString += "&ensp;<i>" + currentDefinition.example + "</i>";
-            }    
+
+    if (json instanceof Array) {
+      try {
+        let dictStrings = [];
+        for (let i=0; i<json.length; i++) {
+          let dict = json[i];
+          let dictString = "";
+          if (json.length > 1) {
+            let supNumber = i+1;
+            // supNumber = superscript(supNumber);
+            dictString = "<b>" + dict.word + "<sup>" + supNumber + "</sup>" + "</b>";
+          } else {
+            dictString = "<b>" + dict.word + "</b>";
           }
+          if ("phonetic" in dict) {
+            dictString += " " + dict.phonetic;
+          }
+          for (let j=0; j<dict.meanings.length; j++) {
+            let meaning = dict.meanings[j];
+            dictString += "<br>(" + meaning.partOfSpeech + ")";
+            for (let k=0; k<meaning.definitions.length; k++) {
+              let currentDefinition = meaning.definitions[k];
+              let definition = currentDefinition.definition;
+              if (meaning.definitions.length > 1) {
+                let definitionNumber = k + 1;
+                dictString += "&ensp;<b>" + definitionNumber + ".</b> " + definition + "";
+              } else {
+                dictString += "&ensp;" + definition + "";
+              }
+              if ("example" in currentDefinition) {
+                dictString += "&ensp;<i>" + currentDefinition.example + "</i>";
+              }    
+            }
+          }
+          dictStrings.push(dictString);
         }
-        dictStrings.push(dictString);
+        dictString = dictStrings.join("<br>");
+        // console.log(dictString);
+      } catch (error) {
+        console.error(error);
+        // console.log("<error on getDictString>");
+        dictString = "（取得できませんでした）";
       }
-      dictString = dictStrings.join("<br>");
-      // console.log(dictString);
-    } catch (error) {
-      console.error(error);
-      // console.log("<error on getDictString>");
+    } else if (json instanceof Object) {
+      if ("title" in json) {
+        if (json.title === "No Definitions Found") {
+          dictString = "（項目がありませんでした）";
+        } else {
+          dictString = "（取得できませんでした）";
+        }
+      }
+    } else {
       dictString = "（取得できませんでした）";
     }
     setDictInfo(parse(dictString));
