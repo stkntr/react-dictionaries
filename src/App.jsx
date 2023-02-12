6c4985bb296ba01cf9351f7f4e13c312732a87b8
Kentaro Sato <stkntr@gmail.com>
2023-02-12 11:13:28 +0900

    おみくじとじゃんけんを削除

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { Omikuji } from "./pages/Omikuji";
-import { Janken } from "./pages/Janken";
+// import { Omikuji } from "./pages/Omikuji";
+// import { Janken } from "./pages/Janken";
 import { EnglishEnglish } from "./pages/English-English";
 import { Urban } from "./pages/Urban";
 // import './App.css'
@@ -9,17 +9,17 @@ const App = () => {
   return (
     <BrowserRouter>
       <header className="flex justify-center p-4">
-        <h1 className="text-3xl font-bold">react app</h1>
+        <h1 className="text-3xl font-bold">react dictionaries</h1>
       </header>
       <main className="max-w-5xl m-auto">
         <section>
           <ul className="text-center">
-            <li>
+            {/* <li>
               <Link to="/omikuji">おみくじ</Link>
             </li>
             <li>
               <Link to="/janken">じゃんけん</Link>
-            </li>
+            </li> */}
             <li>
               <Link to="/english-english">英英辞書</Link>
             </li>
@@ -31,8 +31,8 @@ const App = () => {
         <hr className="mx-8 my-4" />
 
         <Routes>
-          <Route path="/omikuji" element={<Omikuji />} />
-          <Route path="/janken" element={<Janken />} />
+          {/* <Route path="/omikuji" element={<Omikuji />} />
+          <Route path="/janken" element={<Janken />} /> */}
           <Route path="/english-english" element={<EnglishEnglish />} />
           <Route path="/urban" element={<Urban />} />
         </Routes>
