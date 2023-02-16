3391b13af66b4a25f8a74b8f0631f02f076ac1e2
Kentaro Sato <stkntr@gmail.com>
2023-02-16 16:06:31 +0900

    じゃんけんとおみくじとsvgを削除

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-// import { Omikuji } from "./pages/Omikuji";
-// import { Janken } from "./pages/Janken";
 import { EnglishEnglish } from "./pages/English-English";
 import { ChineseEnglish } from "./pages/Chinese-English";
 import { Urban } from "./pages/Urban";
@@ -15,12 +13,6 @@ const App = () => {
       <main className="max-w-5xl m-auto">
         <section>
           <ul className="text-center">
-            {/* <li>
-              <Link to="/omikuji">おみくじ</Link>
-            </li>
-            <li>
-              <Link to="/janken">じゃんけん</Link>
-            </li> */}
             <li>
               <Link to="/english-english">英英辞書</Link>
             </li>
@@ -35,8 +27,6 @@ const App = () => {
         <hr className="mx-8 my-4" />
 
         <Routes>
-          {/* <Route path="/omikuji" element={<Omikuji />} />
-          <Route path="/janken" element={<Janken />} /> */}
           <Route path="/english-english" element={<EnglishEnglish />} />
           <Route path="/chinese-english" element={<ChineseEnglish />} />
           <Route path="/urban" element={<Urban />} />
